0a8f44b3e9d6d0eda63e990274f1c7fea7a81b6d
Jaeho Cho <jaeho2025@gmail.com>
2025-03-05 19:01:23 -0500

    baseurl -> marvin-app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   app: {
-    baseURL: '/test/',
+    baseURL: '/marvin-app/',
   },
   experimental: {
     payloadExtraction: false
